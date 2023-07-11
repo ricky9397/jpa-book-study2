24a785f7752aaf6048d7c9a65148d3d23cf57941
ricky <kimzzan1234@gmail.com>
2023-07-11 23:14:05 +0900

    test

--- bookmanager/src/main/java/com/ricky/jpa/bookmanager/controller/HelloWorldController.java
+++ bookmanager/src/main/java/com/ricky/jpa/bookmanager/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.ricky.jpa.bookmanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,13 @@ public class HelloWorldController {
     @GetMapping("hello-world")
     public String helloWorldController() {
         return "hello-world";
+    }
+
+
+    @PostMapping("/login")
+    public String test() {
+        System.out.println("test sibalruma");
+        return "";
     }
 
 
